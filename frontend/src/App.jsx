9837ec9d3a0fa2981a1ea79825142881ce8803f6
prachi <prachisingh33798@gmail.com>
2026-06-18 13:22:10 +0530

    addes navbar and add links to it for home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
+import Doctors from "./pages/Doctors"
+import Home from "./pages/Home"
+import { Routes, Route } from 'react-router-dom'
+import Login from "./pages/Login"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import MyAppointment from "./pages/MyAppointment"
+import MyProfile from "./pages/MyProfile"
+import Appointment from "./pages/Appointment"
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-red-500">
-        Tailwind Working 
-      </h1>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-appointment' element={<MyAppointment/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
+      </Routes>
     </div>
   )
 }
