22f4e1cbdb664f6fda9eb14cc8bdbddaa26058f8
Samiksha Singh <singhsamiksha350@gmail.com>
2026-06-18 10:38:23 +0530

    Added Tailwind Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const App = () => {
+function App() {
   return (
     <div>
-
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind Working 
+      </h1>
     </div>
   )
 }
