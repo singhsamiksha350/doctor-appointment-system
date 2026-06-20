636db2cbb1609ef626a062de668bb09874e0d8a3
prachi <prachisingh33798@gmail.com>
2026-06-20 15:24:41 +0530

    created till top speciality page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
+import Speciality from '../components/Speciality'
+import TopDoctor from '../components/TopDoctor'
 
 const Home = () => {
   return (
     <div>
-        Homepage
+        <Header/>
+        <Speciality/>
+        <TopDoctor/>
     </div>
   )
 }
